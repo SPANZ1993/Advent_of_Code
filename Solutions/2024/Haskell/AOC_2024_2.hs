b875594f9ddb7c4213d45e29f510f97bc8eb79f9
SPANZ1993 <riggins.spencer@gmail.com>
2024-12-02 11:31:18 -0500

    Minor Cleanup to 2024 Pt. 2 Script

--- Solutions/2024/Haskell/AOC_2024_2.hs
+++ Solutions/2024/Haskell/AOC_2024_2.hs
@@ -11,7 +11,7 @@ isSorted x = x == sort x
 diffsBetween :: (Ord a, Num a) => [a] -> a -> a -> Bool
 diffsBetween [] n1 n2 = True
 diffsBetween [x] n1 n2 = True
-diffsBetween (x1 : x2 : x) n1 n2 = d >= n1 && d <= n2 && diffsBetween (x2 : x) n1 n2
+diffsBetween (x1 : x2 : xs) n1 n2 = d >= n1 && d <= n2 && diffsBetween (x2 : xs) n1 n2
   where
     d = abs $ x1 - x2
 
@@ -26,13 +26,12 @@ main = do
   contents_ <- readFile "../../../Input/2024/2.txt"
   let contents = map words $ lines contents_
   let reports = map readInts contents
-  -- print reports
 
   -- Part 1
-  let nsafe =
+  let nsafe1 =
         length $
           filter (filterSafe 1 3) reports
-  putStrLn $ "Part 1: " ++ show nsafe
+  putStrLn $ "Part 1: " ++ show nsafe1
 
   -- Part 2
   let nsafe2 =
